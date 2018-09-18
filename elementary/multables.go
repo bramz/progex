15a25b8e1da307e34d8e343a0bfe0b235c425281
bramz <brockramz@gmail.com>
2018-09-17 21:41:20 -0400

    Exercise 6, multiplication table of 12

---
+++ elementary/multables.go
@@ -0,0 +1,21 @@
+/*
+Write a program that prints a multiplication table for numbers up to 12.
+*/
+package main
+
+import (
+	"fmt"
+)
+
+const n = 12
+
+func main() {
+	for i := 1; i <= n; i++ {
+		for s := 1; s <= 12; s++ {
+			table := i * s
+			fmt.Print(table)
+			fmt.Printf(" | ")
+		}
+	}
+	fmt.Printf("\n")
+}
